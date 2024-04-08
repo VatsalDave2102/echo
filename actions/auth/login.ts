4c270186ea606d3cae44cc0945da36fecf91fddb
Vatsal Dave <vatsaldave2002@gmail.com>
2024-04-08 11:52:44 +0530

    (feat): add two factor authentication and backend api for same

--- actions/auth/login.ts
+++ actions/auth/login.ts
@@ -7,8 +7,13 @@ import { LoginSchema } from "@/schemas";
 import { getUserByEmail } from "@/data/user";
 import { signIn } from "@/auth";
 import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
-import { generateVerificationToken } from "@/lib/tokens";
-import { sendVerificationEmail } from "@/lib/mail";
+import {
+	generateTwoFactorToken,
+	generateVerificationToken,
+} from "@/lib/tokens";
+import { sendTwoFactorEmail, sendVerificationEmail } from "@/lib/mail";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
+import { getTwoFactorConfirmationByUserId } from "@/data/two-factor-confirmation";
 
 export const login = async (values: z.infer<typeof LoginSchema>) => {
 	// fields validation
@@ -46,6 +51,72 @@ export const login = async (values: z.infer<typeof LoginSchema>) => {
 		);
 
 		return { success: "Confirmation email sent!" };
+	}
+
+	// check if two factor is enabled
+	if (existingUser.isTwoFactorEnabled && existingUser.email) {
+		if (code) {
+			const twoFactorToken = await getTwoFactorTokenByEmail(existingUser.email);
+
+			// if token does not exist
+			if (!twoFactorToken) {
+				return { error: "Invalid code!" };
+			}
+
+			// if token does not match code given by user
+			if (twoFactorToken.token !== code) {
+				return { error: "Invalid conde!" };
+			}
+
+			// checking for expiration of code
+			const hasExpired = new Date(twoFactorToken.expires) < new Date();
+
+			if (hasExpired) {
+				return { error: "Code expired" };
+			}
+
+			// delete token due to expiration
+			await fetch("http://localhost:3000/api/auth/two-factor-token", {
+				method: "DELETE",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify({ id: twoFactorToken.id }),
+			});
+
+			// check for existing confirmation
+			const existingConfirmation = await getTwoFactorConfirmationByUserId(
+				existingUser.id
+			);
+
+			// delete confirmation if exists
+			if (existingConfirmation) {
+				await fetch("http://localhost:3000/api/auth/two-factor-confirmation", {
+					method: "DELETE",
+					headers: {
+						"Content-type": "application/json",
+					},
+					body: JSON.stringify({ id: existingConfirmation.id }),
+				});
+			}
+
+			// create new confirmation
+			await fetch("http://localhost:3000/api/auth/two-factor-confirmation", {
+				method: "POST",
+				headers: {
+					"Content-type": "application/json",
+				},
+				body: JSON.stringify({ userId: existingUser.id }),
+			});
+		} else {
+			// generate new two factor token
+			const twoFactorToken = await generateTwoFactorToken(existingUser.email);
+
+			// send token via email to user
+			await sendTwoFactorEmail(twoFactorToken.email, twoFactorToken.token);
+
+			return { twoFactor: true };
+		}
 	}
 
 	// signing in user
--- lib/tokens.ts
+++ lib/tokens.ts
@@ -1,7 +1,13 @@
+import crypto from "crypto";
 import { v4 as uuidv4 } from "uuid";
 
+import {
+	PasswordResetToken,
+	TwoFactorToken,
+	VerificationToken,
+} from "@prisma/client";
+import { getTwoFactorTokenByEmail } from "@/data/two-factor-token";
 import { getVerificationTokenByEmail } from "@/data/verification-token";
-import { PasswordResetToken, VerificationToken } from "@prisma/client";
 import { getPasswordResetTokenByEmail } from "@/data/reset-password-token";
 
 // function to generate verification token
@@ -40,6 +46,7 @@ export const generateVerificationToken = async (email: string) => {
 	return verificationToken;
 };
 
+// function to generate password reset token
 export const generatePasswordResetToken = async (email: string) => {
 	const token = uuidv4();
 	const expires = new Date(new Date().getTime() + 3600 * 1000);
@@ -71,4 +78,43 @@ export const generatePasswordResetToken = async (email: string) => {
 	const passwordResetToken = await result.json();
 
 	return passwordResetToken as PasswordResetToken;
+};
+
+// function to generate two factor token
+export const generateTwoFactorToken = async (email: string) => {
+	// generate token and expiration time
+	const token = crypto.randomInt(100000, 1000000).toString();
+	const expires = new Date(new Date().getTime() + 5 * 60 * 1000);
+
+	// if already token exist, delete it
+	const exisitingToken = await getTwoFactorTokenByEmail(email);
+
+	if (exisitingToken) {
+		await fetch("http://localhost:3000/api/auth/two-factor-token", {
+			method: "DELETE",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({ id: exisitingToken.id }),
+		});
+	}
+
+	const result = await fetch(
+		"http://localhost:3000/api/auth/two-factor-token",
+		{
+			method: "POST",
+			headers: {
+				"Content-type": "application/json",
+			},
+			body: JSON.stringify({
+				email,
+				token,
+				expires,
+			}),
+		}
+	);
+
+	const twoFactorToken = await result.json();
+
+	return twoFactorToken as TwoFactorToken;
 };
