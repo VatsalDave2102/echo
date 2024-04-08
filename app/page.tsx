cf162e0ce08707930d9327c0cedb7066d31aac58
Vatsal Dave <vatsaldave2002@gmail.com>
2024-04-08 12:07:34 +0530

    (feat): add user button

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,9 @@
-"use client";
-import { logout } from "@/actions/auth/logout";
-import { Button } from "@/components/ui/button";
-import { useSession } from "next-auth/react";
+import UserButton from "@/components/auth/user-button";
 
 export default function Home() {
-	const onClick = async () => {
-		await logout();
-	};
-	const session = useSession();
 	return (
 		<div>
-			<p className="text-3xl font-bold text-rose-500">
-				{session.data?.user?.email}
-			</p>
-			<Button className="bg-rose-500 text-white" onClick={onClick}>
-				Logout
-			</Button>
+			<UserButton />
 		</div>
 	);
 }
